50e3af1a5f4166e9c0a1f5adba82028643c4a689
Edelzio Lopes <edelzio@hotmail.com>
2025-05-16 15:42:21 -0300

    1605-2

--- Application/views/home/index.php
+++ Application/views/home/index.php
@@ -1,12 +1,12 @@
 <h1>Produtos</h1>
-<?php 
-foreach($data['produtos'] as $produto):
-  echo "<br>".$produto['nome'];
-endforeach;
-?>
-<h1>Categorias</h1>
-<?php 
-foreach($data['categorias'] as $categoria):
-  echo "<br>".$categoria['nome'];
-endforeach;
-?>
+<?php foreach($data['produtos'] as $produto): ?>
+  <div class="card" style="width: 18rem;">
+    <img src="<?=$produto['imagem_link']?>" class="card-img-top">
+    <div class="card-body">
+      <h5 class="card-title"><?=$produto['produto_descricao']?></h5>
+      <p class="card-text"><?=$produto['produto_preco']?></p>
+      <p class="card-text"><?=$produto['categoria_nome']?></p>
+      <a href="#" class="btn btn-primary">Go somewhere</a>
+    </div>
+  </div>
+<?php endforeach; ?>
